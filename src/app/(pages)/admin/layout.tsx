59e0ce9992a81b2310911641325f6229aeca1db2
hasham0 <hashamsaleem75@gmail.com>
2024-08-05 15:52:25 +0500

    feat: admin dashboard home page added

--- src/app/(pages)/admin/layout.tsx
+++ src/app/(pages)/admin/layout.tsx
@@ -21,8 +21,11 @@ import {
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "./_components/sidebar";
 import navItems from "@/utils/navigation";
-
-const DashboardLayout = () => {
+import { ReactNode } from "react";
+type DashboardLayoutProps = {
+  children: ReactNode;
+};
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <Sidebar />
@@ -91,23 +94,7 @@ const DashboardLayout = () => {
           </DropdownMenu>
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          <div className="flex items-center">
-            <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
-          </div>
-          <div
-            className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-            x-chunk="dashboard-02-chunk-1"
-          >
-            <div className="flex flex-col items-center gap-1 text-center">
-              <h3 className="text-2xl font-bold tracking-tight">
-                You have no products
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                You can start selling as soon as you add a product.
-              </p>
-              <Button className="mt-4">Add Product</Button>
-            </div>
-          </div>
+          {children}
         </main>
       </div>
     </div>
