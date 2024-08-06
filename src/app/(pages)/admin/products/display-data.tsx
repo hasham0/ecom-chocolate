3d5016c0fca2a23f15876e1861fab1de4ff9e7ac
hasham0 <hashamsaleem75@gmail.com>
2024-08-06 17:26:32 +0500

    feat: add loading spinner while fetching products

--- src/app/(pages)/admin/products/display-data.tsx
+++ src/app/(pages)/admin/products/display-data.tsx
@@ -4,7 +4,8 @@ import { DataTable } from "./data-table";
 import columns from "./columns";
 import { getAllProducts } from "@/app/http/api";
 import { useQuery } from "@tanstack/react-query";
-import { ProductTS } from "@/types";
+import { ProductTS } from "@/lib/validators/productSchema";
+import { Loader2 } from "lucide-react";
 
 type Props = {};
 
@@ -19,7 +20,11 @@ const DisplayData = (props: Props) => {
   });
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex h-64 items-center justify-center">
+        <Loader2 className="size-10 animate-spin" />
+      </div>
+    );
   }
 
   if (isError) {
